9a27d23d30203f796ba55cd91a68b16e16db4772
Omollo Erick <ericotieno99@gmail.com>
2019-05-24 10:37:25 +0300

    changed sms send time

--- app/twilio.go
+++ app/twilio.go
@@ -26,15 +26,16 @@ func init() {
 func SendSMSMessages(message chan models.MessageDetails) {
 	for {
 		message := <-message
+		toNumber := "+254723436438"
 		// Set account keys & information
 
 		if message.MessageID > 0 {
-			dateTime, _ := checkSentMessages("+254723436438")
+			data, _ := checkSentMessages(toNumber)
 			t1 := time.Now()
-			diff := t1.Sub(dateTime)
+			diff := t1.Sub(data.DateTime)
 			dif := int64(diff.Minutes())
-			fmt.Println(dif)
-			if dif > 30 {
+			fmt.Println(dif, data.MessageID)
+			if (dif > 10 && data.MessageID == message.MessageID) || (data.MessageID != message.MessageID) {
 				accountSid := "ACeab16ebd80a48c1f4318f09c6ad6e33e"
 				authToken := "8812492c587bf5cda4ee01a0bfedff3d"
 				urlStr := "https://api.twilio.com/2010-04-01/Accounts/" + accountSid + "/Messages.json"
@@ -78,17 +79,24 @@ func SendSMSMessages(message chan models.MessageDetails) {
 					fmt.Println(resp.Status)
 				}
 			}
+
 		}
 	}
 }
 
-// check if user has sent message in the last 5min
-func checkSentMessages(tonumber string) (time.Time, error) {
-	var datetime time.Time
-	q := DBCon.NewQuery("SELECT date_time FROM saved_messages WHERE `to`='" + tonumber + "' ORDER BY id DESC  LIMIT 1 ")
-	err := q.Row(&datetime)
+// SMSCheck ...
+type SMSCheck struct {
+	MessageID int       `json:"message_id"`
+	DateTime  time.Time `json:"date_time"`
+}
 
-	return datetime, err
+// check if user has sent message in the last 5min
+func checkSentMessages(tonumber string) (SMSCheck, error) {
+	var data SMSCheck
+	q := DBCon.NewQuery("SELECT date_time, message_id FROM saved_messages WHERE `to`='" + tonumber + "' ORDER BY id DESC  LIMIT 1 ")
+	err := q.One(&data)
+
+	return data, err
 }
 
 // save sent messages
