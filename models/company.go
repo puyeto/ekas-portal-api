c2007962794d1506638d90c61a9feb8687cb7ba8
Omollo Erick <ericotieno99@gmail.com>
2019-07-25 11:38:04 +0300

    Added company details

--- models/company.go
+++ models/company.go
@@ -4,15 +4,22 @@ import validation "github.com/go-ozzo/ozzo-validation"
 
 // Companies ...
 type Companies struct {
-	CompanyID       int    `json:"company_id" db:"pk,company_id"`
-	CompanyName     string `json:"company_name" db:"company_name"`
-	CompanyContacts string `json:"company_contacts,omitempty"`
-	UserID          int32  `json:"user_id,omitempty"`
+	CompanyID          int    `json:"company_id" db:"pk,company_id"`
+	CompanyName        string `json:"company_name" db:"company_name"`
+	CompanyContacts    string `json:"company_contacts"`
+	CompanyContactName string `json:"company_contact_name"`
+	CompanyEmail       string `json:"company_email"`
+	CompanyLocation    string `json:"company_location"`
+	UserID             int32  `json:"user_id,omitempty"`
 }
 
-// ValidateDevices validates user data fields.
+// ValidateCompanies validates user data fields.
 func (m Companies) ValidateCompanies() error {
 	return validation.ValidateStruct(&m,
 		validation.Field(&m.CompanyName, validation.Required),
+		validation.Field(&m.CompanyContactName, validation.Required),
+		validation.Field(&m.CompanyContacts, validation.Required),
+		validation.Field(&m.CompanyEmail, validation.Required),
+		validation.Field(&m.CompanyLocation, validation.Required),
 	)
 }
