b8c074321e6c04a17c93b60c68b98ec30dd66a4e
Omollo Erick <ericotieno99@gmail.com>
2019-08-06 12:25:47 +0300

    vehicle owner & fitter fixes

--- daos/vehicle_agents.go
+++ daos/vehicle_agents.go
@@ -12,7 +12,7 @@ import (
 
 // CreateFitter Add Fitter
 func (dao *VehicleDAO) CreateFitter(rs app.RequestScope, fd *models.FitterDetails) error {
-	exists, _ := dao.FitterExists(rs, fd.FitterID)
+	exists, _ := dao.FitterExists(rs, fd.FitterIDNo)
 	if exists == 1 {
 		return dao.UpdateFitter(rs, fd)
 	}
@@ -30,14 +30,14 @@ func (dao *VehicleDAO) UpdateFitter(rs app.RequestScope, fd *models.FitterDetail
 		"fitter_phone":        fd.FitterPhone,
 		"fitting_date":        fd.FittingDate,
 		"fitter_biz_reg_no":   fd.FitterBizRegNo},
-		dbx.HashExp{"fitting_id": fd.FitterID}).Execute()
+		dbx.HashExp{"fitting_id_no": fd.FitterIDNo}).Execute()
 	return err
 }
 
 // FitterExists check if fitter exists
 func (dao *VehicleDAO) FitterExists(rs app.RequestScope, id uint32) (int, error) {
 	var exists int
-	q := rs.Tx().NewQuery("SELECT EXISTS(SELECT 1 FROM fitter_details WHERE fitting_id='" + strconv.Itoa(int(id)) + "' LIMIT 1) AS exist")
+	q := rs.Tx().NewQuery("SELECT EXISTS(SELECT 1 FROM fitter_details WHERE fitting_id_no='" + strconv.Itoa(int(id)) + "' LIMIT 1) AS exist")
 	err := q.Row(&exists)
 	return exists, err
 }
--- daos/vehicle_owners.go
+++ daos/vehicle_owners.go
@@ -12,7 +12,7 @@ import (
 
 // CreateVehicleOwner Add vehicle owner
 func (dao *VehicleDAO) CreateVehicleOwner(rs app.RequestScope, vo *models.VehicleOwner) error {
-	exists, _ := dao.VehicleOwnerExists(rs, vo.OwnerID)
+	exists, _ := dao.VehicleOwnerExists(rs, vo.OwnerIDNo)
 	if exists == 1 {
 		return dao.UpdateVehicleOwners(rs, vo)
 	}
@@ -26,14 +26,14 @@ func (dao *VehicleDAO) UpdateVehicleOwners(rs app.RequestScope, vo *models.Vehic
 		"user_id":     vo.UserID,
 		"owner_email": vo.OwnerEmail,
 		"owner_phone": vo.OwnerPhone},
-		dbx.HashExp{"owner_id": vo.OwnerID}).Execute()
+		dbx.HashExp{"owner_id_no": vo.OwnerIDNo}).Execute()
 	return err
 }
 
 // VehicleOwnerExists ...
 func (dao *VehicleDAO) VehicleOwnerExists(rs app.RequestScope, id uint32) (int, error) {
 	var exists int
-	q := rs.Tx().NewQuery("SELECT EXISTS(SELECT 1 FROM vehicle_owner WHERE owner_id='" + strconv.Itoa(int(id)) + "' LIMIT 1) AS exist")
+	q := rs.Tx().NewQuery("SELECT EXISTS(SELECT 1 FROM vehicle_owner WHERE owner_id_no='" + strconv.Itoa(int(id)) + "' LIMIT 1) AS exist")
 	err := q.Row(&exists)
 	return exists, err
 }
