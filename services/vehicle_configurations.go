5d86ffd56cfcd34b1e91d9e2e30b032ee5222220
Omollo Erick <ericotieno99@gmail.com>
2019-11-01 03:33:43 +0300

    Search by id Read from redis

--- services/vehicle_configurations.go
+++ services/vehicle_configurations.go
@@ -64,7 +64,27 @@ func (s *VehicleService) CountTripDataByDeviceID(deviceid string) (int, error) {
 
 // GetTripDataByDeviceID ...
 func (s *VehicleService) GetTripDataByDeviceID(deviceid string, offset, limit int) ([]models.DeviceData, error) {
-	return s.dao.GetTripDataByDeviceID(deviceid, offset, limit)
+	var deviceData []models.DeviceData
+
+	keysList, err := app.ZRevRange("data:"+deviceid, int64(offset), int64(limit-1))
+	if err != nil {
+		fmt.Println("Getting Keys Failed : " + err.Error())
+	}
+
+	for i := 0; i < len(keysList); i++ {
+
+		if keysList[i] != "0" {
+			var deserializedValue models.DeviceData
+			json.Unmarshal([]byte(keysList[i]), &deserializedValue)
+			deviceData = append(deviceData, deserializedValue)
+		}
+
+	}
+	if len(deviceData) == 0 {
+		return s.dao.GetTripDataByDeviceID(deviceid, offset, limit)
+	}
+
+	return deviceData, err
 }
 
 // CountTripRecordsBtwDates ...
